e68de09f3b6a279f799ad4927b7515059dd12805
eatbuildplay <61839698+eatbuildplay@users.noreply.github.com>
2020-03-05 10:43:54 -0500

    create plugin declaration file

---
+++ plugin-declaration.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * For the full list of plugin declaration options see https://developer.wordpress.org/plugins/plugin-basics/header-requirements/
+ *
+ * Fun fact, there is only 1 required option that is actually needed to active a WordPress plugin and that's "Plugin Name".
+ * In practice professional developers are encouraged to create a complete and accurate updated plugin declaration.
+ *
+ */
+
+/**
+ *
+ * Plugin Name: Big Fishes
+ * Plugin URI: https://wp-developer-site.com/plugins/big-fishes/
+ * Description: Does stuff involving fishes, big fishes no less and also example of minimal WP plugin definition.
+ * Version: 1.0.0
+ * Author: Suzanne Devperson, Developer Company
+ * Author URI: https://wp-developer-site.com/
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ */
