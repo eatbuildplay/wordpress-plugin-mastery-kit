643c1c31d09307aa0a8b0382a87b5c4bb9e11329
eatbuildplay <61839698+eatbuildplay@users.noreply.github.com>
2020-03-05 11:34:57 -0500

    create file

---
+++ plugin-constants.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+ * In this example replace WC_ONSITE_COURSES with the fully capitalized and underscored name of your plugin.
+ * While there are other constants you may want to set, these will be the 3 that enable you to start including PHP files and enqueuing JS/CSS files
+ */
+
+define( 'WC_ONSITE_COURSES_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+define( 'WC_ONSITE_COURSES_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'WC_ONSITE_COURSES_VERSION', '1.0.0' );
