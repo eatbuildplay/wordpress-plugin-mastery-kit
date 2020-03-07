036ed8405b800dddb53ec21541a3aa94b3244b86
eatbuildplay <61839698+eatbuildplay@users.noreply.github.com>
2020-03-07 10:46:08 -0500

    init

---
+++ jquery-wrapper.js
@@ -0,0 +1,6 @@
+(function($) {
+	
+	// $ works here now that we've enclosed the jQuery in a function
+  // enjoy writing $('#element') instead of the cumbersome jQuery('#element') with this approach
+	
+})( jQuery );
