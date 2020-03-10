eea40c23e8fd1e45013dae68e9403a7fcfe48aa1
Eat/Build/Play <61839698+eatbuildplay@users.noreply.github.com>
2020-03-10 11:05:11 -0500

    init

---
+++ js-form-validators.js
@@ -0,0 +1,12 @@
+function validateRequired( el ) {
+    if( el.val() === '' ) {
+      return false
+    }
+    return true
+  }
+
+  function validateEmail( el ) {
+    let email = el.val()
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
